5e1638a6bc597830d176d66610ba0efc78eb2452
Chris Howard <chris@connectivity.systems>
2023-12-05 23:12:24 +0800

    Added test for full input

--- day5/day5_test.go
+++ day5/day5_test.go
@@ -10,11 +10,11 @@ func TestPart1WithExampleInput(t *testing.T) {
 
 	defer file.Close()
 
-	part1Location := findLocation(file, 1)
+	location := findLocation(file, 1)
 	// part2Location := findLocation(file, 2)
 
-	if part1Location != 35 {
-		t.Errorf("Result was incorrect, got: %d, want: %d.", part1Location, 35)
+	if location != 35 {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", location, 35)
 	}
 	// if part2Location != 46 {
 	// 	t.Errorf("Result was incorrect, got: %d, want: %d.", part2Location, 46)
@@ -26,10 +26,10 @@ func TestPart2WithExampleInput(t *testing.T) {
 
 	defer file.Close()
 
-	part1Location := findLocation(file, 2)
+	location := findLocation(file, 2)
 
-	if part1Location != 46 {
-		t.Errorf("Result was incorrect, got: %d, want: %d.", part1Location, 46)
+	if location != 46 {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", location, 46)
 	}
 }
 
@@ -38,9 +38,21 @@ func TestPart1WithFullInput(t *testing.T) {
 
 	defer file.Close()
 
-	part1Location := findLocation(file, 1)
+	location := findLocation(file, 1)
 
-	if part1Location != 218513636 {
-		t.Errorf("Result was incorrect, got: %d, want: %d.", part1Location, 218513636)
+	if location != 218513636 {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", location, 218513636)
+	}
+}
+
+func TestPart2WithFullInput(t *testing.T) {
+	file, _ := os.Open("input.txt")
+
+	defer file.Close()
+
+	location := findLocation(file, 2)
+
+	if location != 81956384 {
+		t.Errorf("Result was incorrect, got: %d, want: %d.", location, 81956384)
 	}
 }
